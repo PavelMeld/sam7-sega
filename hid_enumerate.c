fca55fcb4d7215885138a8b50c8762d11386531f
PavelMeld <pavel.meld@gmail.com>
2020-05-03 17:13:29 +0300

    Small fixes, added compiled binary

--- hid_enumerate.c
+++ hid_enumerate.c
@@ -47,6 +47,7 @@ const unsigned char langDescrKeyboard[] = {
 	'D', 0x00,
 };
 
+
 const unsigned char langDescrKeyboardSino[] = {
 	0x18, // bLength
 	0x03, // Descriptor Type (3)
@@ -63,42 +64,6 @@ const unsigned char langDescrKeyboardSino[] = {
 	'H', 0x00
 };
 
-
-char joypadDescriptor_mpv[] = {
-    0x05, 0x01,                    // USAGE_PAGE (Generic Desktop) +
-
-    0x09, 0x04,                    // USAGE (Joystick)	+
-    0xa1, 0x01,                    // COLLECTION (Application) +
-
-	0x85, 0x01,                    //   REPORT 1
-    0xa1, 0x02,                    //     COLLECTION (Logical)
-	0x75, 0x08,                    //       REPORT_SIZE (8)
-    0x95, 0x02,                    //       REPORT_COUNT (2)
-    0x15, 0x00,                    //         LOGICAL_MINIMUM (0)
-    0x26, 0xff, 0x00,              //         LOGICAL_MAXIMUM (255)
-    0x35, 0x00,                    //         PHYSICAL_MINIMUM (0)
-    0x46, 0xff, 0x00,              //         PHYSICAL_MAXIMUM (255)
-
-    0x95, 0x02,                    //       REPORT_COUNT (2)
-    0x09, 0x30,                    //         USAGE (X)
-    0x09, 0x31,                    //         USAGE (Y)
-    0x81, 0x02,                    //         INPUT (Data,Var,Abs)
-	0x65, 0x00,                    //         UNITS (?)
-
-
-// 8-bit buttons
-	0x75, 0x01,                    //       REPORT_SIZE (1)
-    0x95, 0x08,                    //       REPORT_COUNT (8)
-	0x25, 0x01,                    //         LOGICAL_MAXIMUM (1)
-    0x45, 0x01,                    //         PHYSICAL_MAXIMUM (1)
-	0x05, 0x09,                    //         USAGE_PAGE (Button)
-	0x19, 0x01,                    //         USAGE_MINIMUM (Button 1)
-	0x29, 0x08,                    //         USAGE_MMAXIMUM (Button 8)
-    0x81, 0x02,                    //         INPUT (Data,Var,Abs)
-
-	0xC0,                          //     END COLLECTION
-	0xC0                           //   END COLLECTION
-};
 
 char joypadDescriptor[] = {
     0x05, 0x01,                    // USAGE_PAGE (Generic Desktop) +
